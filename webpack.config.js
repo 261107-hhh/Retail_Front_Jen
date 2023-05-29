f7742cc0a94baf489dd53bccaf2a906a309a8b81
Himanshu Nainwal <himanshu.nainwal@STLERI.COM>
2023-05-29 11:29:43 +0530

    Jenkins

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-module: {
-    rules: [
-        {
-            test: /\.(js|jsx)$/i,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        '@babel/preset-env',
-                        '@babel/preset-react',
-                    ],
-                },
-            },
-        },
-        // ... Your other rules
+// module: {
+//     rules: [
+//         {
+//             test: /\.(js|jsx)$/i,
+//             exclude: /node_modules/,
+//             use: {
+//                 loader: 'babel-loader',
+//                 options: {
+//                     presets: [
+//                         '@babel/preset-env',
+//                         '@babel/preset-react',
+//                     ],
+//                 },
+//             },
+//         },
+//         // ... Your other rules
 
-    ],
-}
+//     ],
+// }
 
