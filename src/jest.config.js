77bd3621ffeb0e605ebf904719269e6861ddc284
Himanshu Nainwal <himanshu.nainwal@STLERI.COM>
2023-05-30 11:55:34 +0530

    test

--- src/jest.config.js
+++ src/jest.config.js
@@ -4,7 +4,15 @@ const jestConfig = {
     'transform': {
       '^.+\\.jsx?$': 'babel-jest',
     },
-    testMatch: ['**/__tests__/*.js?(x)'],
+    // testMatch: ['**/__tests__/*.js?(x)'],
+    "testMatch": [
+        // "<rootDir>/src/**/*.(test).{js,jsx,ts,tsx}",
+        "<rootDir>/src/__tests__/*.(test).{js,jsx,ts,tsx}",
+        // "<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}"
+      ],
   }
   
   module.exports = jestConfig
+
+
+  
